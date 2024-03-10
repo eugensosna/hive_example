cd3447a71bfbfb1442af13ea3e6528c42450675c
Evhen Sosna <eugensosna@gmx.de>
2024-03-10 20:31:37 +0100

    dependes

---
+++ lib/person.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+import 'package:hive/hive.dart';
+
+part 'person.g.dart';
+
+@HiveType(TypeId: 1)
+class Person {
+  Person({
+    required this.name,
+    required this.age,
+  })
+
+
+  @HiveField(1)
+  String name;
+
+  @HiveField(2)
+  int age;
+
+}
